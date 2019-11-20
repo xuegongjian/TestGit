5f6842c3c601f93155daa1c49f8c9309be21f1fb
xuegongjian <xuegongjian@xuegongjian-pc>
2019-11-21 00:38:28 +0800

    fdsafdsaf

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,13 +8,14 @@ public class Test {
 		i =4;
 		System.out.println(i>>>1);
 	}
-	private	void	a(int	d){
+	private	void	a(int	dsafds){
 		int	b=0;
 	}
 	
-	private	void	d(){
+	private	void	d(	){
 		int	b=0;
 	}
+	
 	
 	public	int		c(){
 		return	0;
